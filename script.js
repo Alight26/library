821208844d4e50967af85cd0929b67ecd9082017
Lucas <luc.coulson6@gmail.com>
2025-06-25 17:14:08 -0400

    books can be created in the form and pushed to array

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const myLibrary = [];
 
-function Book(author, title, number, read) {
+function Book(title, author, number, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.number = number;
+    this.read = Boolean(read);
   // the constructor...
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, numberOfPages, isRead) {
   // take params, create a book then store it in the array
+
+    const newBook = new Book(title, author, numberOfPages, isRead);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    
 }
 
 
 // button to activate the form
 const btn = document.querySelector("#add"); 
 btn.addEventListener("click", () => {
-    alert("button Clicked");
+    const form = document.querySelector("#add-book");
+    form.classList.remove("hidden");
+
+
 
 });
+
+const submit = document.querySelector(".submit-button");
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const numberOfPages = parseInt(document.querySelector("#number-of-pages").value);
+    const isRead = document.querySelector("#read").checked;
+    addBookToLibrary(title, author, numberOfPages, isRead);
+
+    
+});
+
+
 
 
