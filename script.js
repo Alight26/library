082b7d805d680e4d1292b759552119af7c16796d
Lucas <luc.coulson6@gmail.com>
2025-06-25 12:28:58 -0400

    added a form and made the add button work with js

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(author, title, number, read) {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+
+// button to activate the form
+const btn = document.querySelector("#add"); 
+btn.addEventListener("click", () => {
+    alert("button Clicked");
+
+});
+
+
